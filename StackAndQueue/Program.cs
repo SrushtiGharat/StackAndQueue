203dba182a4b7b096d3321609323fd9880ef3604
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-22 17:39:07 +0530

    Add project files.

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack Program");
+        }
+        
+    }
+}
